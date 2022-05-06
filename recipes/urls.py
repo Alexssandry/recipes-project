2540ddd49ddc6724dbcc807ca7ff34c1860a9daa
Alexssandry Rafael Pereira <alexssandry@hotmail.com>
2022-05-06 16:53:46 -0300

    usando app_name na views

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from . import views
 
+# recipes:recipe
+app_name = 'recipes'
+
 urlpatterns = [
-    path('', views.view_home),
-    path('recipes/<int:id>/', views.view_recipe),
+    path('', views.view_home, name='home'),
+    path('recipes/<int:id>/', views.view_recipe, name='recipe'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 # Create your views here.
 
 
 def view_home(request):
     context = {
-        'title': 'Recipes',
-        'name': 'Alexssandry',
+        'recipes': [make_recipe() for _ in range(10)],
     }
     return render(request, 'recipes/pages/home.html', context=context)
 
 
 def view_recipe(request, id):
     context = {
-        'title': 'Recipes Details',
+        'recipe': make_recipe(),
+        'is_detail_page': True,
     }
     return render(request, 'recipes/pages/recipe-view.html', context=context)
